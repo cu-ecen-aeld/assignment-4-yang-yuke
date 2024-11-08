f9d0fc2e86d34ca03986c955e3c9b9ca6eef07b5
yang-yuke <yang1294891635@gmail.com>
2024-11-08 09:14:43 -0800

    point to the commit of assignment3-and-later to make finder-test.sh execute anywhere

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ce5410408bc638dc88e9fd82705a5832390d9f76
+AESD_ASSIGNMENTS_VERSION = 3bf03acd1d218dad14994b6dad43f71eb5d5f34c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
